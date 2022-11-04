80baddce9ed8031f213efafbf501dc49df3f11fe
user.name like:"hamza khalid <user.email like :hamzakhalid10002000@gmail.com>
2022-11-04 20:00:06 +0500

    new development branch

--- lib/main.dart
+++ lib/main.dart
@@ -1,16 +1,10 @@
 import 'package:easy_localization/easy_localization.dart';
 
-<<<<<<< HEAD
 
-import 'package:flutter/cupertino.dart';
-import 'package:tradingsignals/Screens/homeScreen.dart';
-import 'package:tradingsignals/Screens/shopScreen.dart';
-import 'package:tradingsignals/Screens/signalsScreen.dart';
-import 'package:tradingsignals/Screens/tipsScreen.dart';
 import 'package:flutter/material.dart';
 
 import 'package:tradingsignals/Screens/startupscreen.dart';
->>>>>>> f1d4d20c959f4d90f88983652f4c95829cd61c63
+
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -38,11 +32,8 @@ class MyApp extends StatelessWidget {
   const MyApp({super.key});
   @override
   Widget build(BuildContext context) {
-    return CupertinoApp(
-      title: 'Flutter Demo',
-      
-      home:SignalsScreen(),
-======
+
+
     return MaterialApp(
       localizationsDelegates: context.localizationDelegates,
       supportedLocales: context.supportedLocales,
